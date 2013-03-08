6ba91a5349a8aad1ac19263c216862dec29259a7
jtfairbank <jtfairbank@gmail.com>
2013-03-08 01:54:15 -0600

    Update requestHelper.dart to reflect changes in Dart.

--- src/lib/requestHelper.dart
+++ src/lib/requestHelper.dart
@@ -4,8 +4,8 @@ import "dart:uri";
 
 class RequestHelper {
   static String encodeMap(Map data) {
-    return Strings.join(data.keys.map((k) {
+    return data.keys.map((k) {
       return '${encodeUriComponent(k)}=${encodeUriComponent(data[k])}';
-    }), '&');
+    }).join('&');
   } 
 }
