863524d9ffa5802ec6c07c81b9b6d95a4b91ee66
Jungho Kwon <kwoner61@gmail.com>
2013-06-23 09:00:41 -0500

    Web-UI: Moving Player Create state to webui.

--- src/view_models/PlayerCreateState.dart
+++ src/view_models/PlayerCreateState.dart
@@ -1,23 +1,23 @@
 part of udjlib;
 
-class PlayerCreateState extends UIState {
+class PlayerCreateState {
   
-  final UdjApp _udjApp;
+  final UdjApp _udj;
   
-  PlayerCreateState(this._udjApp):super();
+  PlayerCreateState(this._udj);
   
   void formSubmit(Map playerAttrs) {
-    _udjApp.service.createPlayer(playerAttrs, (Map status) {
+    _udj.service.createPlayer(playerAttrs, (Map status) {
       if (status['success']) {
         Player p = new Player.fromJson( status['playerData'] );
         
-        _udjApp.service.setPlayerState(p.id, 'paused', (Map status) {
+        _udj.service.setPlayerState(p.id, 'paused', (Map status) {
           if (status['success']) {
-            _udjApp.service.addPlayerLibrary(p.id, "1", (Map status) {
+            _udj.service.addPlayerLibrary(p.id, "1", (Map status) {
               if (status['success']) {
-                _udjApp.state.localPlayer.value = p;
-                _udjApp.state.currentPlayer.value = p;
-                _udjApp.state.creatingPlayer.value = false;
+                //_udj.state.localPlayer = p;
+                _udj.state.currentPlayer = p;
+                _udj.state.creatingPlayer = false;
                 
               } else {
                 // TODO: error handling
